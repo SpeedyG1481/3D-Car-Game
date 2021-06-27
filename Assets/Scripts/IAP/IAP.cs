075961678f8ce1e994710c6a741b2411fe43e2a5
ozgen_3032@hotmail.com <Yusuf.1481>
2021-06-27 12:39:06 +0300

    v0.3.8
    In App Purchase problem fixed.

--- Assets/Scripts/IAP/IAP.cs
+++ Assets/Scripts/IAP/IAP.cs
@@ -2,19 +2,26 @@
 using UnityEngine.Purchasing;
 
 [RequireComponent(typeof(AudioSource))]
-public class IAP : MonoBehaviour
+public class IAP : MonoBehaviour, IStoreListener
 {
     private static readonly string KnightRider = "com.megalowgamestudio.firtf.knightrider";
     private static readonly string Fustang = "com.megalowgamestudio.firtf.fustang";
 
 
+    private IStoreController _controller;
+    private IExtensionProvider _extensions;
     private AudioSource _audioSource;
+
     [SerializeField] private AudioClip success;
     [SerializeField] private AudioClip wrong;
 
     private void Start()
     {
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("com.megalowgamestudio.firtf.knightrider", ProductType.NonConsumable);
+        builder.AddProduct("com.megalowgamestudio.firtf.fustang", ProductType.NonConsumable);
         _audioSource = GetComponent<AudioSource>();
+        UnityPurchasing.Initialize(this, builder);
     }
 
     public void OnPurchaseComplete(Product product)
@@ -33,9 +40,25 @@ public class IAP : MonoBehaviour
         }
     }
 
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        this._controller = controller;
+        this._extensions = extensions;
+    }
+
     public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
     {
         _audioSource.volume = GameController.GetSfxVolume;
         _audioSource.PlayOneShot(wrong, GameController.GetSfxVolume);
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
     }
 }
